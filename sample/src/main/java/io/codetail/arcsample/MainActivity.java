1064fb521d0c596dca93a8090695689a6411e151
AndroidDeveloperLB <lblb636@gmail.com>
2018-11-17 14:44:16 +0200

    updated sdks

--- sample/src/main/java/io/codetail/arcsample/MainActivity.java
+++ sample/src/main/java/io/codetail/arcsample/MainActivity.java
@@ -1,11 +1,11 @@
 package io.codetail.arcsample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
